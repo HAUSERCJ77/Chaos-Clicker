f3f8bae1afe788c1300c940325c84ae5f7ad7048
CJ Hauser <hausercj77@gmail.com>
2025-09-19 20:37:49 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,23 @@
-const cacheName='chaos-clicker-v1';
-const filesToCache=['/','/index.html','/click.mp3','/manifest.json'];
+const cacheName = 'chaos-clicker-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/click.mp3',
+  '/manifest.json'
+];
 
 self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(filesToCache)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache=>{
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(resp=>resp||fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response=>{
+      return response || fetch(e.request);
+    })
+  );
 });
